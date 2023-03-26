1a79367b65a9b79d3afc22334ca845ae8307b5cf
Kaese <charliegen@hotmail.se>
2023-03-26 20:11:18 +0200

    Added more valid non default log leves to stdoutlogger

--- logging/standardlogger.go
+++ logging/standardlogger.go
@@ -10,7 +10,9 @@ type StandardOutLogger struct {
 func (log StandardOutLogger) Log(msg string, priority int, datas ...map[string]string) {
 	label, ok := map[int]string{
 		7: "DEBUG",
-		3: "ERROR",
+		6: "INFO",
+		4: "WARNING",
+		// Default: 3: "ERROR",
 	}[priority]
 	if !ok {
 		label = "ERROR"
